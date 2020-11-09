d69ccc6bacb6979222bfbbdc8a869d6a49d5dded
Nate Bernstein <nbernstein@chapman.edu>
2020-11-09 11:53:55 -0700

    Adds username.sh

--- username.sh
+++ username.sh
@@ -6,10 +6,10 @@ echo "Use lower case characters, digits and underscore only. "
 echo "It must start with a lower case character."
 echo "It must contain at least 3 but not more than 12 characters" 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z,_,0-9]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a username!"
-	echo "Enter a five-digit ZIP code: "
+	echo "Enter a username: "
 	read USERNAME
 done
 echo "Thank you"
