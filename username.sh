ff8da6507d07433f999125fd6fc2fb586f1d1d16
Nate Bernstein <nbernstein@chapman.edu>
2020-11-16 11:09:54 -0700

    Updates username.sh

--- username.sh
+++ username.sh
@@ -6,7 +6,7 @@ echo "Use lower case characters, digits and underscore only. "
 echo "It must start with a lower case character."
 echo "It must contain at least 3 but not more than 12 characters" 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][0-9,_,a-z]{3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][0-9,_,a-z]{2,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a username!"
 	echo "Enter a username: "
